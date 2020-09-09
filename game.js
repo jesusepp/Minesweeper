afbf0c6c9f05e6975c49d200fa95833e8736b06d
jesusepp <jesusepereira@gmail.com>
2020-09-08 23:07:19 -0400

    Update

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ class Game {
         } else if (this.diff == 2){
             this.Xdim = 20;
             this.Ydim = 15;
-            this.diffMult = 10;
+            this.diffMult = 5;
         }
         this.mineGen();
         this.numGen();
